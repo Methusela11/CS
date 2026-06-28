ef1cb198c4f48cf15b58a29fdc55f0c5555873e1
Methusela11 <soenkilifi@gmail.com>
2026-06-28 09:14:17 +0300

    full/8

---
+++ full/8/crack.py
@@ -0,0 +1,11 @@
+from string import ascii_letters, digits, punctuation
+
+for i in ascii_letters + digits + punctuation:
+    for j in ascii_letters + digits + punctuation:
+        for k in ascii_letters + digits + punctuation:
+            for l in ascii_letters + digits + punctuation:
+                for m in ascii_letters + digits + punctuation:
+                    for n in ascii_letters + digits + punctuation:
+                        for o in ascii_letters + digits + punctuation:
+                            for p in ascii_letters + digits + punctuation:
+                            print(i, j, k, l, m, n, o, p)
