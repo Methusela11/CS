048f0102c3b6952e4666ba037306bef75f9445fa
Methusela11 <soenkilifi@gmail.com>
2026-06-29 22:07:52 +0300

    letters/5

--- letters/6/crack.py
+++ letters/6/crack.py
@@ -5,4 +5,5 @@ for i in ascii_letters:
         for k in ascii_letters:
             for l in ascii_letters:
                 for m in ascii_letters:
-                    print(i, j, k, l, m)
+                    for n in ascii_letters:
+                        print(i, j, k, l, m, n)
