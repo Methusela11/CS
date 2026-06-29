7c03c74731314fd5ba4f5160572c721d79d7417e
Methusela11 <soenkilifi@gmail.com>
2026-06-29 22:10:02 +0300

    letters/8

---
+++ letters/8/crack.py
@@ -0,0 +1,11 @@
+from string import ascii_letters
+
+for i in ascii_letters:
+    for j in ascii_letters:
+        for k in ascii_letters:
+            for l in ascii_letters:
+                for m in ascii_letters:
+                    for n in ascii_letters:
+                        for o in ascii_letters:
+                            for p in ascii_letters:
+                                print(i, j, k, l, m, n, o, p)
